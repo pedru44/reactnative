99305ecacd8e9f3b2621fe7bd8d9cc7103142a66
Pedro Henrique Fernandes de Carvalho <pedroc@iesb.edu.br>
2023-08-21 20:48:15 -0300

    projeto incial conceitos

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Card from './components/Card';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView style={{margin:10}}>
+      <Card></Card>
+      
+    <View style={styles.card}>
+      <Text style={styles.titulo}>Hello World!</Text>
+      <Text> isto é um parágrafo.</Text>
+      <Text style={styles.titulomenor}> isto é um parágrafo.</Text>
+      <Text> isto é um parágrafo.</Text>
       <StatusBar style="auto" />
     </View>
+    
+    </ScrollView>
+    
   );
 }
 
@@ -17,4 +29,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    borderColor: 'black',
+    bordeStyle: 'solid',
+    borderWidth: 2,
+  },
+  titulo: {
+    color: 'black',
+    fontSize: 30,
+    padding: 20,
+    marginBottom: 20,
+    borderBottomColor: 'red',
+    bordeBottomStyle: 'solid',
+    bordeBottomWidth: 2,
+    textAlign: 'center',
+    backgroundColor: 'green',
+
+  },
+  titulomenor: {
+    fontSize:10
+  }
 });
